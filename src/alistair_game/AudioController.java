b4ebcab24c49cff5dc26141f47dc3c5ea30fbc4c
Matthew Lui <gamma032@gmail.com>
2019-01-01 16:16:14 +1100

    Finished AudioController

--- src/alistair_game/AudioController.java
+++ src/alistair_game/AudioController.java
@@ -1,22 +1,43 @@
 package alistair_game;
 
 import org.newdawn.slick.*;
+import java.util.*;
 
 public class AudioController  {
     /** Handles the game audio.
      * Initialized in App.init.
      * To play a sound, call play(event) */
 
-    private static Sound[] intro = new Sound[1];
-    private static Sound[] gameover = new Sound[1];
+    private static Sound[] intro = new Sound[2];
+    private static Sound[] gameover = new Sound[2];
+    private static Sound victory, quickSort, python, mergeSort, insertionSort, commerceStudent, alcohol;
+
     AudioController() {
-        // Initialize sounds
         try {
-            intro[0] = new Sound("assets\\Audio\\Intro.ogg");
+            String path = "assets\\Audio\\";
 
-            gameover[0] = new Sound("assets\\Audio\\GameOver.ogg");
+            // Sounds
+            intro[0] = new Sound(path+"Intro.ogg");
+            intro[1] = new Sound(path+"Intro2.ogg");
+            gameover[0] = new Sound(path+"GameOver.ogg");
+            gameover[1] = new Sound(path+"Gameover2.ogg");
 
-            // TODO: Add the rest of the audio files
+            Sound victory = new Sound(path + "Victory.ogg");
+
+            Sound alcohol = new Sound(path + "Alcohol.ogg");
+            Sound commerceStudent = new Sound(path + "CommerceStudent.ogg");
+            Sound python = new Sound(path + "Python.ogg");
+
+            Sound insertionSort = new Sound(path + "InsertionSort.ogg");
+            Sound mergeSort = new Sound(path + "MergeSort.ogg");
+            Sound quickSort = new Sound(path + "QuickSort.ogg");
+
+            // Music
+            // TODO: Find some music? Can add an array of songs to a musicLoop() method.
+            /*
+            Music bgmusic = new Music(Path + "");
+            bgmusic.loop();
+            */
         } catch (SlickException e) {
             e.printStackTrace();
         }
@@ -27,16 +48,33 @@ public class AudioController  {
         // TODO: Add pitch and volume control
         // Intro sound
         switch (event) {
-            // TODO: Add a randomizer
             case "intro":
-                intro[0].play();
+                intro[rand(2)].play();
                 break;
             case "gameover":
-                gameover[0].play();
+                gameover[rand(2)].play();
                 break;
+            case "victory":
+                victory.play();
+                break;
+            case "quicksort":
+                quickSort.play();
+                break;
+            case "insertionsort":
+                insertionSort.play();
+                break;
+            case "mergesort":
+                mergeSort.play();
+                break;
+            case "python":
+                python.play();
         }
     }
 
-    // TODO: Add an audio-state checker
-    // TODO: Add a music handler
+    private static int rand(int sounds) {
+        /* Generates an int 0- (sounds-1) */
+        Random gen = new Random(System.nanoTime());
+        int choice = gen.nextInt(sounds);
+        return choice;
+    }
 }
