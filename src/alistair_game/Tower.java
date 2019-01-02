a4d56be88fe804f771bb116af1832de66db5aed3
Matthew Lui <gamma032@gmail.com>
2019-01-02 19:26:46 +1100

    Fixed a Graphics Overwrite Bug
    Resets colours after drawing the elipse, allowing everything else to be drawn properly.

--- src/alistair_game/Tower.java
+++ src/alistair_game/Tower.java
@@ -20,13 +20,23 @@ class Tower extends Sprite {
 
 	void drawRange(Graphics g) {
 		/* Draws a range circle around towers */
+		Color oldcol = g.getColor();
+
 		// Top-left corner of the circle
 		float xcorner = getX() - range, ycorner = getY() - range;
+
 		// Draw circumference
-		g.drawOval(xcorner, ycorner, range*2, range*2);
+		g.setColor(new Color(110, 110, 110,110));
+		g.drawOval(xcorner, ycorner, range*2, range*2
+		);
 		// Fill with a shade of grey (can change vals depending on contrast w/ textures)
 		g.setColor(new Color(80, 80, 80, 80));
 		g.fillOval(xcorner, ycorner, range*2, range*2);
+
+		// Reset color
+		// MATT: How do colours work, and why does not resetting it break other graphics operations?
+		g.setColor(oldcol);
+
 	}
 
 	boolean isPlaced() {
