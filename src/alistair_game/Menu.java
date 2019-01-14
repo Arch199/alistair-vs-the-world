3e9bd4528ac8e9072da647c3b1f75b14be626fb6
Lawrence Leong <lsleong@student.unimelb.edu.au>
2019-01-14 22:13:30 +0800

    Addition of Option Boxes

--- src/alistair_game/Menu.java
+++ src/alistair_game/Menu.java
@@ -2,6 +2,7 @@ package alistair_game;
 
 import java.awt.Font;
 import org.newdawn.slick.Color;
+import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.TrueTypeFont;
 
@@ -33,6 +34,8 @@ public class Menu {
      * @return The action taken (e.g. "Quit" to exit game). Defaults to the empty string.
      */
     String update(Input input) {
+        int xpos = input.getMouseX(), ypos = input.getMouseY();
+        
         if (input.isKeyPressed(Input.KEY_DOWN)) {
             if (currentChoice == choices.length-1) {
                 currentChoice = 0;
@@ -50,6 +53,9 @@ public class Menu {
         else if (input.isKeyPressed(Input.KEY_ENTER)) {
             return choices[currentChoice];
         }
+        else if () { // TODO: Add Keyhover and press function
+            
+        } 
         return ""; // default value for no action
     }
     
@@ -57,12 +63,17 @@ public class Menu {
         TITLE_TTF.drawString((w/2) - (TITLE_TTF.getWidth(title)/2), 125, title, TITLE_COL);
     }
     
-    void renderOptions() {
+    void renderOptions(Graphics g) {
         for (int i = 0; i < choices.length; i++) {
             if (currentChoice == i) {
-                OPTION_TTF.drawString((w/2) - OPTION_TTF.getWidth(choices[i])/2, i*50+250, choices[i], CHOSEN_COL);
+                OPTION_TTF.drawString((w/2) - OPTION_TTF.getWidth(choices[i])/2, i*56+252, choices[i], CHOSEN_COL);
+                g.setColor(CHOSEN_COL);
+                g.drawRect((w/2) - (OPTION_TTF.getWidth(choices[i])/2)-2, i*56 + 250, OPTION_TTF.getWidth(choices[i]) + 4, OPTION_TTF.getHeight() + 4);
             } else {
-                OPTION_TTF.drawString((w/2) - OPTION_TTF.getWidth(choices[i])/2, i*50+250, choices[i], NOT_CHOSEN_COL);
+                OPTION_TTF.drawString((w/2) - OPTION_TTF.getWidth(choices[i])/2, i*56+252, choices[i], NOT_CHOSEN_COL);
+                g.setColor(NOT_CHOSEN_COL);
+                g.drawRect((w/2) - (OPTION_TTF.getWidth(choices[i])/2)-2, i*56 + 250, OPTION_TTF.getWidth(choices[i]) + 4, OPTION_TTF.getHeight() + 4);
+                //TODO: Replace magic numbers with Constants
             }
         }
     }
