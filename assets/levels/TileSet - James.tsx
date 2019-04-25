12112084dc34ee89ead9f6061d15b8cb210ce446
Lawrence Leong <lsleong@student.unimelb.edu.au>
2019-04-25 20:07:31 +1000

    Implemented TiledMap

---
+++ assets/levels/TileSet - James.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="TileSet - Jmaes" tilewidth="48" tileheight="48" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="3" type="Land_Tile">
+  <properties>
+   <property name="isWall" value="true"/>
+  </properties>
+  <image width="48" height="48" source="../sprites/tiles/wall.png"/>
+ </tile>
+ <tile id="5" type="Alistair_HQ">
+  <properties>
+   <property name="isWall" value="false"/>
+  </properties>
+  <image width="48" height="48" source="../sprites/tiles/alistair.png"/>
+ </tile>
+ <tile id="6" type="Normal_Path_Tile">
+  <properties>
+   <property name="isWall" value="false"/>
+  </properties>
+  <image width="48" height="48" source="../sprites/tiles/path.png"/>
+ </tile>
+</tileset>
