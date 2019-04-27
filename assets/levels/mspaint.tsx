c231259f11ca0f0a3817d43272b194865231d334
James Dyer <jamesdyer199@gmail.com>
2019-04-28 02:17:51 +1000

    Reformat tiled map files

---
+++ assets/levels/mspaint.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="mspaint" tilewidth="48" tileheight="48" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1" type="Alistair">
+  <properties>
+   <property name="isWall" value="false"/>
+  </properties>
+  <image width="48" height="48" source="../sprites/tiles/alistair.png"/>
+ </tile>
+ <tile id="2" type="Wall">
+  <properties>
+   <property name="isWall" value="true"/>
+  </properties>
+  <image width="48" height="48" source="../sprites/tiles/wall.png"/>
+ </tile>
+ <tile id="3" type="Path">
+  <properties>
+   <property name="isWall" value="false"/>
+  </properties>
+  <image width="48" height="48" source="../sprites/tiles/path.png"/>
+ </tile>
+</tileset>
