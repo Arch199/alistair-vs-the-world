d6101978316cce772a677def4885b5444e89a7f9
Matthew Lui <gamma032@gmail.com>
2019-07-05 22:14:41 +1000

    Implemented a 'holds defence' property

--- assets/levels/custom_tileset.tsx
+++ assets/levels/custom_tileset.tsx
@@ -3,6 +3,7 @@
  <image source="alistair-cropped.jpg" width="64" height="64"/>
  <tile id="0">
   <properties>
+   <property name="holdsDefence" type="bool" value="false"/>
    <property name="isWall" type="bool" value="false"/>
   </properties>
  </tile>
